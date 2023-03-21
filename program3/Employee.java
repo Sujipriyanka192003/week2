cdf5eabd1f4827d1af8c1df8967ff6c556115122
Sujipriyanka192003 <123354092+Sujipriyanka192003@users.noreply.github.com>
2023-03-21 10:46:25 +0530

    Add files via upload

---
+++ program3/Employee.java
@@ -0,0 +1,8 @@
+public class Employee {
+    public void detials(String name,int year,String address,int salary){
+        System.out.println(name+"\t\t "+year+"\t\t\t"+address);
+
+    }
+
+}
+
---
+++ program3/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Name\tYear of joining\t\tAddress");
+
+        Employee e1=new Employee();
+        e1.detials("Robert",1994,"64c-Wallsmart",50000);
+        Employee e2=new Employee();
+        e2.detials("Sam  ",2000,"68D-Wallsmart",20000);
+        Employee e3=new Employee();
+        e3.detials("John",1999,"26B-Wallsmart",60000);
+
+
+    }
+}
